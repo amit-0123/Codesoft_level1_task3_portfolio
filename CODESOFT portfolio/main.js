58a4e199653285523ba10d6a17fb54a4b51718be
amit-0123 <128837907+amit-0123@users.noreply.github.com>
2023-07-31 12:16:10 +0530

    Add files via upload

---
+++ CODESOFT portfolio/main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Web Developer" , "Coding Enthusiastic" , "Data Scientist"],
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true
+});
